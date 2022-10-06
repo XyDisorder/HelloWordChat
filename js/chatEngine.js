b3d2b1e27c8aa4bbe38a6c861ea48872916e7714
Sarah BOURGEOIS <sarah.bourgeois.bc@gmail.com>
2022-10-06 14:58:53 +0200

    feat(chatEngine): change design and display text in each box

---
+++ js/chatEngine.js
@@ -0,0 +1,33 @@
+function SendMessage(sender) {
+    let message = "";
+
+    if (sender === 1) {
+        message = document.getElementById("text_user1").value;
+        SendMessageToReceiver(message, "chatbox2");
+        AddMessageForSender(message, "chatbox1");
+    } else if (sender === 2) {
+        message = document.getElementById("text_user2").value;
+        SendMessageToReceiver(message, "chatbox1");
+        AddMessageForSender(message, "chatbox2");
+    }
+}
+
+function SendMessageToReceiver(message, targetId) {
+    // create a new element
+    const el = document.createElement('li');
+    el.classList.add('receiver');
+    // Add text content to element
+    el.textContent = message;
+    // add element to DOM
+    const box = document.getElementById(targetId);
+    box.appendChild(el);
+}
+
+function AddMessageForSender(message, targetId) {
+    const el = document.createElement('li');
+    el.classList.add('sender');
+    el.textContent = message;
+    const box = document.getElementById(targetId);
+    box.appendChild(el);
+}
+
