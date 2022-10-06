4cd94be8602a5757d979376784b25f67f8cdb7b3
Sarah BOURGEOIS <sarah.bourgeois.bc@gmail.com>
2022-10-06 04:26:39 +0200

    feat(chatEngine): change design and display text in each box

--- js/chatengine.js
+++ js/chatengine.js
@@ -1,12 +1,11 @@
 function SendMessage(sender) {
-    let message = "";
-   // document.getElementById("text_user1").value = sender;
+    let message = "yo";
 
-    if (sender == 1) {
+    if (sender === 1) {
         message = document.getElementById("text_user1").value;
         AddMessage(message, "chatbox2", "box2");
     }
-    else if (sender == 2) {
+    else if (sender === 2) {
         message = document.getElementById("text_user2").value;
         AddMessage(message, "chatbox1", "box1");
     }
@@ -14,17 +13,15 @@ function SendMessage(sender) {
 
 function AddMessage(message, id, targetId) {
     const el = document.createElement('div');
-
-// Set ID attribute on element
+    // Set ID attribute on element
     el.setAttribute('id', id);
 
-// Add text content to element
+    // Add text content to element
     el.textContent = message;
 
-// Or set the innerHTML of the element
-// el.innerHTML = `<span>Hello world</span>`;
-
-// add element to DOM
+    // add element to DOM
     const box = document.getElementById(targetId);
     box.appendChild(el);
 }
+
+
